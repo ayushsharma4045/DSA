2e05fdd2227c4b04da672575ca6d3b6145006e84
ayushsharma4045 <ayushsharma4045@gmail.com>
2024-06-04 12:06:40 +0530

    Create 90. Subsets II.java

---
+++ 90. Subsets II.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    static List<List<Integer>> res = new ArrayList<>();
+    public static void main(String[] args)throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = in.readLine().trim().split(" ");
+        int nums[] = new int[s.length];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=Integer.parseInt(s[i]);
+        }
+        System.out.println(subsetsWithDup(nums));
+    }
+    
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        back(0,nums,new ArrayList<>());
+        return res;
+    }
+    public static void back(int ind,int[] nums,List<Integer> temp){
+        res.add(new ArrayList<>(temp));
+        for(int i=ind;i<nums.length;i++){
+            if(i>ind && nums[i]==nums[i-1]){
+                continue;
+            }
+            temp.add(nums[i]);
+            back(i+1,nums,temp);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
