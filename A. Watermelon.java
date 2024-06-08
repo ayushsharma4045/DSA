2fa0dc37194c8170d13cf0ad3e229cba4e212988
ayushsharma045 <ayushsharma4045@gmail.com>
2024-06-09 00:41:46 +0530

    Create A. Watermelon.java

---
+++ A. Watermelon.java
@@ -0,0 +1,18 @@
+/*
+ * Problem Link: https://codeforces.com/problemset/problem/4/A
+ */
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+ 
+        if (x < 4) {
+            System.out.println("NO");
+        } 
+        else {
+            System.out.println(x % 2 == 0 ? "YES" : "NO");
+        }
+    }
+}
